bde480106e9b5dc37b41387febed549e379458c6
窦朋 <724488033@qq.com>
2016-12-16 15:41:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@
 
 from setuptools import setup,find_packages
 
-
 setup(
       name="dpspider",
-      version="1.0.0",
-      description="A lightweight Web Crawling and Web Scraping framework",
+      version="2.0.1",
+      description="A lightweight Web Crawling and Web Scraping framework.(v2.x.x add distributed processing)",
       author="doupeng",
       author_email = 'doupeng1993@sina.com',
       url="https://github.com/doupengs/dpspider",
