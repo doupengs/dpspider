62cf7c5a2219bec4718e632cc700880d7bf46fb1
窦朋 <724488033@qq.com>
2017-01-01 11:05:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
 #coding:utf-8
 
-from setuptools import setup,find_packages
+from setuptools import setup
+from dpspider import __version__
 
 setup(
-      name="dpspider",
-      version="2.0.1",
-      description="A lightweight Web Crawling and Web Scraping framework.(v2.x.x add distributed processing)",
-      author="doupeng",
-      author_email = 'doupeng1993@sina.com',
-      url="https://github.com/doupengs/dpspider",
-      license="GPL",
-      packages= find_packages(),
-      install_requires=[
-          'requests>=2.10',
-          'lxml>=3.6',
-          'MySQL-python>=1.2.3',
-          'redis>=2.10'
-          ]
-      )
+    name="dpspider",
+    version=__version__,
+    description="A Web Crawling and Web Scraping framework.",
+    author="doupeng",
+    author_email = 'doupeng1993@sina.com',
+    url="https://github.com/doupengs/dpspider",
+    license="GPL",
+    packages= ['dpspider'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Web Scraping Development :: Python Modules',
+        'License :: GPL License',
+        'Programming Language :: Python :: 2.7',
+        ],
+    install_requires=[
+        'requests>=2.10',
+        'lxml>=3.6',
+        'MySQL-python>=1.2.3',
+        'redis>=2.10'
+      ]
+    )
